8c6d730c0d61baec39e794befa03cca45dd63717
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-07-26 14:01:25 +1000

    feat: Create BaseLayout component and apply the layer and providers in App.tsx

---
+++ src/components/layout/BaseLayout.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { useAuth } from '../../context/auth-context';
+
+const userOnlyPath = [
+  '/my-orders',
+  '/account-settings',
+  '/my-account',
+  '/saved-items',
+];
+
+export default function BaseLayout({ children }: { children: ReactNode }) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (!user && userOnlyPath.includes(location.pathname)) {
+      navigate('/signin');
+    }
+  }, [user]);
+
+  return <main className='mt-16 lg:mt-24'>{children}</main>;
+}
