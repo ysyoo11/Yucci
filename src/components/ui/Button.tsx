b5153e6c5478466b5c19363e811cb8787de2bcfe
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-08-02 22:17:22 +1000

    fix: Add disabled props in Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,19 +4,26 @@ interface Props {
   className?: string;
   children: string;
   full?: boolean;
+  disabled?: boolean;
 }
 
-export default function Button({ className, children, full = false }: Props) {
+export default function Button({
+  className,
+  children,
+  full = false,
+  disabled = false,
+}: Props) {
   return (
     <button
       className={clsx(
-        'bg-black px-8 py-5 font-bold uppercase text-white transition-opacity hover:opacity-80 lg:text-base',
+        'bg-black px-8 py-5 font-bold uppercase text-white transition-opacity hover:opacity-80 disabled:cursor-not-allowed disabled:bg-gray-300 lg:text-base',
         className,
         {
           'w-full text-base': full,
           'text-sm': !full,
         }
       )}
+      disabled={disabled}
     >
       {children}
     </button>
