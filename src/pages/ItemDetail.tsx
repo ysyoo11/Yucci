cbd565ca24bc7d3365599b122970de93686e01eb
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-08-04 01:50:50 +1000

    fix: Add UI for selecting size options in ItemDetail

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Loading from '../components/core/Loading';
@@ -9,6 +10,7 @@ import { getProductDetail } from '../service/firebase';
 import displayPrice from '../utils/display-price';
 
 export default function ItemDetail() {
+  const [option, setOption] = useState<string | undefined>(undefined);
   const { id } = useParams();
   const { data: product, isLoading } = useQuery(
     [QUERY_KEY.PRODUCT_DETAIL, id],
@@ -20,22 +22,50 @@ export default function ItemDetail() {
   );
   const { addToCart } = useCartContext();
 
+  useEffect(() => {
+    if (product) {
+      setOption(product.options[0]);
+    }
+  }, [product]);
+
   if (isLoading || !product) {
     return <Loading />;
   }
 
   return (
     <div className='mx-auto max-w-sm px-4 md:max-w-lg lg:flex lg:max-w-7xl'>
-      <div className='py-10 lg:w-full'>
+      <div className='py-6 lg:w-full'>
         <img src={product.imageURL} alt={product.title} />
       </div>
-      <div className='lg:w-full'>
+      <div className='space-y-2 lg:w-full'>
         <p className='text-center font-bold uppercase'>{product.title}</p>
         <p className='text-center font-bold'>{displayPrice(product.price)}</p>
-        <Button full className='mt-10' onClick={() => addToCart(product)}>
-          add to cart
-        </Button>
-        <p className='mt-6'>{product.description}</p>
+        <div className='flex items-center justify-center space-x-2'>
+          <label htmlFor='option'>Option:</label>
+          <select
+            name='option'
+            id='option'
+            className='border border-gray-200 px-2 py-1 text-sm disabled:bg-gray-300'
+            onChange={(e) => setOption(e.target.value)}
+            required
+            value={option}
+          >
+            {product.options.map((option, idx) => (
+              <option key={`option-${idx}`}>{option}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <Button
+            full
+            className='mt-10'
+            onClick={() => option && addToCart(product, option)}
+            disabled={option === undefined}
+          >
+            add to cart
+          </Button>
+          <p className='mt-6'>{product.description}</p>
+        </div>
       </div>
     </div>
   );
