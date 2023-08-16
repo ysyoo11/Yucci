4570ed803d4f0592642fa9369a30844cf84a6462
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-08-17 04:37:14 +1000

    feat: Create parseDateTime util

---
+++ src/utils/parse-date-time.ts
@@ -0,0 +1,25 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+] as const;
+
+export default function parseDateTime(dateTime: string) {
+  const d = new Date(dateTime);
+  const year = d.getFullYear();
+  const month = months[d.getMonth()];
+  const date = d.getDate();
+  const hour = d.getHours();
+  const min = d.getMinutes();
+
+  return `${date} ${month}, ${year} ${hour}:${min}`;
+}
