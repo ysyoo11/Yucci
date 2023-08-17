17555c2b9fe0909201b2e0f2aaeb983a0ecb0b26
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-08-17 17:34:54 +1000

    refactor: Refactor TextInput and NumberInput by making it combined Input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { ChangeEvent } from 'react';
+import { ComponentPropsWithoutRef } from 'react';
 
 const colorClasses = {
   black:
@@ -8,27 +8,16 @@ const colorClasses = {
     'border-b border-gray-700 text-white focus:border-white focus:outline-none',
 } as const;
 
-type Props = {
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  id: string;
-  value: string;
-  isOptional?: boolean;
-  placeholder: string;
-  disabled?: boolean;
-  name: string;
+type Props = ComponentPropsWithoutRef<'input'> & {
   color?: keyof typeof colorClasses;
 };
 
-export default function TextInput({
-  onChange,
-  id,
-  value,
-  isOptional = false,
-  placeholder,
-  disabled = false,
-  name,
+export default function Input({
+  required = true,
   color = 'black',
+  ...props
 }: Props) {
+  const { id, disabled, placeholder } = props;
   return (
     <div
       className={clsx('relative z-0 w-full', {
@@ -36,25 +25,20 @@ export default function TextInput({
       })}
     >
       <input
-        type='text'
-        id={id}
         className={clsx(
           'peer block w-full appearance-none bg-transparent px-2 pb-4 pt-6 text-base',
           colorClasses[color]
         )}
         placeholder=' '
-        value={value}
-        onChange={onChange}
-        disabled={disabled}
-        name={name}
-        required={!isOptional}
+        required={required}
+        {...props}
       />
       <label
         htmlFor={id}
         className='absolute left-3 top-5 -z-10 origin-[0] -translate-y-4 scale-75 transform text-base text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-gray-600'
       >
         {placeholder}
-        {!isOptional && '*'}
+        {required && '*'}
       </label>
     </div>
   );
